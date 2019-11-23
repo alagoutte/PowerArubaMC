573a4342a36f0498806080d2ffa08d825afef190
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-11-23 23:17:27 +0100

    PowerArubaMC(.psm1): Add AP Provisionning folder

--- PowerArubaMC/PowerArubaMC.psm1
+++ PowerArubaMC/PowerArubaMC.psm1
@@ -1,9 +1,10 @@
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$APProvisionning = @( Get-ChildItem -Path $PSScriptRoot\Public\APProvisionning\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Private)) {
+Foreach ($import in @($Public + $APProvisionning + $Private)) {
     Try {
         . $import.fullname
     }
